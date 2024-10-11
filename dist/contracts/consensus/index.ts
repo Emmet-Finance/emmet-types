7c7c7d92ea5d1eff8a34b9600b81cd9848e7be76
Mithras <dimabrook79@gmail.com>
2024-10-11 18:08:07 +0300

    add: BridgeExplorer, IConsensus

--- dist/contracts/consensus/index.ts
+++ dist/contracts/consensus/index.ts
@@ -4,5 +4,6 @@
 import type * as relayerStakesSol from "./RelayerStakes.sol";
 export type { relayerStakesSol };
 export type { Consensus } from "./Consensus";
+export type { IConsensus } from "./IConsensus";
 export type { Signatures } from "./Signatures";
 export type { TestEmmetToken } from "./TestEmmetToken";
--- dist/factories/contracts/consensus/index.ts
+++ dist/factories/contracts/consensus/index.ts
@@ -3,5 +3,6 @@
 /* eslint-disable */
 export * as relayerStakesSol from "./RelayerStakes.sol";
 export { Consensus__factory } from "./Consensus__factory";
+export { IConsensus__factory } from "./IConsensus__factory";
 export { Signatures__factory } from "./Signatures__factory";
 export { TestEmmetToken__factory } from "./TestEmmetToken__factory";
